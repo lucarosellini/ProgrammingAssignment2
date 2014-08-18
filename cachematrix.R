402b80229df037ce1889d7ac8cb3549569b71341
Luca Rosellini <luca.rosellini@gmail.com>
2014-08-18 11:54:27 +0200

    Assignment 2 completed, tested with simple 2x2 and 4x4 matrices

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,69 @@
 
 ## Write a short comment describing this function
 
+## As in the provided example, makeCacheMatrix wraps the
+## provided matrix 'x' using a special list that exposes
+## four methods:
+## 1. set: sets the values of the matrix
+## 2. get: returns the value of the matrix
+## 3. setinverse: sets the inverse of the matrix
+## 4. getinverse: returns the inverse of the matrix (using the cached value, if exists)
+##
 makeCacheMatrix <- function(x = matrix()) {
-
+    # the inverse value, initialized with NULL
+    inv <- NULL
+    
+    # sets the value of the matrix, called 'm'
+    set <- function(m){
+        x <<- m
+        
+        # if a new matrix is provided, the cached value for the inverse must be invalidated
+        inv <<- NULL
+    }
+    
+    # returns the matrix
+    get <- function(){
+        x
+    }
+    
+    # getting the cached value for the inverse of the matrix 
+    getinverse <- function(){
+        inv
+    }
+    
+    setinverse <- function(inverse){
+        inv <<- inverse
+    }
+    
+    list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)
 }
 
 
 ## Write a short comment describing this function
 
+## the following function contains the logic needed to calculate and to cache
+## the inverse of the matrix wrapped using the function above.
+##
+## In the case we have a cached value for a matrix, before returning it we must
+## check the provided matrix is the same matrix we used to calculate the cached inverse value.
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## Return a matrix that is the inverse of 'x'
+    
+    inv <- x$getinverse()
+    
+    if (!is.null(inv)){
+        message("Returning the cached inverse matrix")
+        return(inv)
+    }
+    
+    ## No cached inverse has been found, let's calc the inverse
+    
+    # getting the matrix for which we need to calc the inverse
+    m <- x$get()
+    
+    # calc the inverse propagating any additional parameter the user may have provided
+    inv <- solve(m, ...)
+    x$setinverse(inv)
+    
+    inv
 }
